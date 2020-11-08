03f87cba7971dd2c2d0b01bebc96e03e62086c53
danvarga1985 <danvarga1985@gmail.com>
2020-11-08 10:07:58 +0100

    Tests

--- src/test/java/com/calendar/controllers/EntryControllerTest.java
+++ src/test/java/com/calendar/controllers/EntryControllerTest.java
@@ -2,8 +2,10 @@ package com.calendar.controllers;
 
 import com.calendar.data.enums.EntryPhase;
 import com.calendar.data.enums.EntryType;
+import com.calendar.exceptions.EntryNotFoundException;
 import com.calendar.responsedto.EntryResponseDto;
 import com.calendar.service.impl.EntryServiceImpl;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -12,16 +14,14 @@ import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.mockito.junit.jupiter.MockitoSettings;
 import org.mockito.quality.Strictness;
+import org.springframework.boot.test.json.JacksonTester;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.mock.web.MockHttpServletResponse;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 
-import java.time.LocalDateTime;
-
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.mockito.ArgumentMatchers.anyInt;
 import static org.mockito.BDDMockito.given;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 
@@ -37,9 +37,11 @@ class EntryControllerTest {
     @InjectMocks
     EntryController entryController;
 
+    private JacksonTester<EntryResponseDto> jsonEntryDto;
+
     @BeforeEach
     void setUp() {
-        Object[] controllers;
+        JacksonTester.initFields(this, new ObjectMapper());
         mockMvc = MockMvcBuilders.standaloneSetup(entryController)
                 .setControllerAdvice(new ErrorControllerAdvice())
                 .build();
@@ -47,13 +49,18 @@ class EntryControllerTest {
 
     @Test
     void testGetEntryByIdWhenExists() throws Exception {
+        // TODO: implement date-formatting
+//        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd@HH:mm");
+//        String time1 = "2019-03-27@10:15";
+//        LocalDateTime localTimeObj1 = LocalDateTime.parse(time1, formatter);
+
         EntryResponseDto responseDto = new EntryResponseDto(1, 1, "ResultDto", "A test Dto",
-                LocalDateTime.now(), null, null, EntryType.PROJECT, EntryPhase.WIP, false,
+                null, null, null, EntryType.PROJECT, EntryPhase.WIP, false,
                 false, 0, false, true, null, 1,
                 "ResultDto");
 
         // given
-        given(entryService.getEntryById(anyInt()))
+        given(entryService.getEntryById(1))
                 .willReturn(responseDto);
 
         // when
@@ -64,5 +71,32 @@ class EntryControllerTest {
 
         //then
         assertThat(response.getStatus()).isEqualTo(HttpStatus.OK.value());
+        assertThat(response.getContentAsString()).isNotEmpty();
+        assertThat(response.getContentAsString()).isEqualTo(jsonEntryDto.write(new EntryResponseDto(1, 1,
+                "ResultDto", "A test Dto", null, null, null,
+                EntryType.PROJECT, EntryPhase.WIP, false, false, 0, false,
+                true, null, 1, "ResultDto")).getJson());
+    }
+
+    @Test
+    public void testGetEntryByIdWhenDoesNotExist() throws Exception {
+        // given
+        given(entryService.getEntryById(2))
+                .willThrow(new EntryNotFoundException("Entry not found!"));
+
+        // when
+        MockHttpServletResponse response = mockMvc.perform(
+                get("/entry/2")
+                        .accept(MediaType.APPLICATION_JSON))
+                .andReturn().getResponse();
+
+        // then
+        assertThat(response.getStatus()).isEqualTo(HttpStatus.NOT_FOUND.value());
+        assertThat(response.getContentAsString()).isEmpty();
+    }
+
+    @Test
+    void testGetProjects() {
+
     }
 }
